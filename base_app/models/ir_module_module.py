a3033b2c4147290db0ff9ca04885860594351714
Henrik Norlin <henrik.norl@gmail.com>
2025-11-09 10:06:23 +0100

    base_app: monetary related to app_module_version

--- base_app/models/ir_module_module.py
+++ base_app/models/ir_module_module.py
@@ -30,7 +30,7 @@ class IrModuleModule(models.Model):
         string="Minutes worked",
         related="module_version_ids.minutes_worked",
     )
-    year_price = fields.Float(
+    year_price = fields.Monetary(
         string="Price: minutes*20/100*sqrt(users)",
         related="module_version_ids.current_price",
         help="Latest valid price per user from the App Price List."
@@ -39,13 +39,12 @@ class IrModuleModule(models.Model):
         string="Migration Work (estimated minutes)",
         related="module_version_ids.minutes_mig_to_next_version",
     )
-    cost_mig_to_next_version = fields.Float(
+    cost_mig_to_next_version = fields.Monetary(
         string="Cost to migrate",
         related="module_version_ids.cost_mig_to_next_version",
     )
-    # currency_id = fields.Many2one(
-    #     "res.currency",
-    #     string="Currency",
-    #     required=True,
-    #     default=lambda self: self.env.company.currency_id.id,
-    # )
+    currency_id = fields.Many2one(
+        "res.currency",
+        string="Currency",
+        related="module_version_ids.currency_id",
+    )
