3dc46b354f599dd7493d7f6c1ed8d82b22202539
Henrik Norlin <henrik@loym.com>
2026-04-20 17:49:03 +0200

    [ADD] base_time_parameter, account_time_parameter

---
+++ account_time_parameter/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021-2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "account_time_parameter",
+    "summary": "",
+    "author": "Ows, Odoo Community Association (OCA)",
+    "category": "Accounting",
+    "data": [
+        "views/base_time_parameter_views.xml",
+    ],
+    "depends": [
+        "account",
+        "base_time_parameter",
+    ],
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "version": "19.0.1.0.0",
+    "website": "https://github.com/OCA",
+}
